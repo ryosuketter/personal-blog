c1c0aa6e97bde37ef69c4d63743dbf740f724055
ryosuketter <r.shimizu.yale@gmail.com>
2023-03-23 08:24:23 +0900

    add: Hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,5 +1,19 @@
 import { FC } from 'react'
 
-export const Hero: FC = () => {
-  return null
+import styles from './style.module.scss'
+interface HeroProps {
+  title?: string
+  subtitle?: string
+}
+
+export const Hero: FC<HeroProps> = ({ title, subtitle }) => {
+  return (
+    <div className={styles.wrapper}>
+      <h2 className={styles.title}>
+        <span className={styles.keyVisual}>/</span>
+        {title}
+      </h2>
+      <span className={styles.subtitle}>{subtitle}</span>
+    </div>
+  )
 }
