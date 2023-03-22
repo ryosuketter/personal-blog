0490d71f9c5d38b12da6d9c6dfc0a974b4c5ad5c
ryosuketter <r.shimizu.yale@gmail.com>
2023-03-23 08:26:23 +0900

    change: Logo セマンティック

--- src/components/Header/Logo/index.tsx
+++ src/components/Header/Logo/index.tsx
@@ -5,8 +5,8 @@ import styles from './style.module.scss'
 
 export const Logo: FC = () => {
   return (
-    <Link href="/" className={styles.logo}>
-      office Shimizu
+    <Link href="/">
+      <h1 className={styles.logo}>office Shimizu</h1>
     </Link>
   )
 }
