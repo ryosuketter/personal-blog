d1ee37c17fb73c203ac791b3ac0b71bd5258bdef
ryosuketter <r.shimizu.yale@gmail.com>
2023-03-24 01:05:56 +0900

    fix: Hero Component

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,17 +3,15 @@ import { FC } from 'react'
 import styles from './style.module.scss'
 interface HeroProps {
   title?: string
-  subtitle?: string
 }
 
-export const Hero: FC<HeroProps> = ({ title, subtitle }) => {
+export const Hero: FC<HeroProps> = ({ title }) => {
   return (
     <div className={styles.wrapper}>
       <h2 className={styles.title}>
         <span className={styles.keyVisual}>/</span>
         {title}
       </h2>
-      <span className={styles.subtitle}>{subtitle}</span>
     </div>
   )
 }
