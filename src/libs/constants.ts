c86df290c452b0ced771b0ebf34a3e4d4ab338f7
ryosuketter <r.shimizu.yale@gmail.com>
2023-03-25 00:58:14 +0900

    change: siteMeta.siteUrl

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -1,7 +1,7 @@
 export const siteMeta = {
   siteTitle: 'office Shimizu',
   siteDesc: 'office Shimizuに関するサイト',
-  siteUrl: 'https://*********',
+  siteUrl: 'https://personal-blog-rho-seven.vercel.app/',
   siteLang: 'ja',
   siteLocale: 'ja_JP',
   siteType: 'website',
