8673a4d4cd044fdb817940e201e48552fc327665
ryosuketter <r.shimizu.yale@gmail.com>
2023-04-01 12:30:24 +0900

    feat: verify API request headers

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -3,9 +3,22 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // eslint-disable-next-line no-console
   console.log('再検証！')
+
+  // if check http request header named x-microcms-signature
+  if (req.headers['x-microcms-signature'] !== process.env.MICROCMS_SIGNATURE) {
+    return res.status(401).send('Unauthorized')
+  }
+  // eslint-disable-next-line no-console
+  console.log('401ではない')
+
   try {
     await res.revalidate('/projects')
-    return res.status(200).send('Revalidated')
+    return res.status(200).json({
+      message: 'Revalidated',
+      hd: req.headers['x-microcms-signature'],
+      bools: req.headers['x-microcms-signature'] !== process.env.MICROCMS_SIGNATURE
+    })
+    // return res.status(200).send('Revalidated')
   } catch (err) {
     return res.status(500).send('Error revalidating')
   }
