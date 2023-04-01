83c304100e3fe71ece23ac055306a8388678b15f
ryosuketter <r.shimizu.yale@gmail.com>
2023-04-01 17:42:44 +0900

    検証 'abc'

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -9,41 +9,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   // eslint-disable-next-line no-console
   console.log('process.env.MICROCMS_SIGNATURE', process.env.MICROCMS_SIGNATURE)
 
-  const signature = req.headers['x-microcms-signature']
+  const signature = req.headers['x-microcms-signature'] as string
 
   // eslint-disable-next-line no-console
   console.log('req.body', req.body)
   // eslint-disable-next-line no-console
   console.log('req.headers', req.headers)
 
-  const expectedSignature = crypto
-    .createHmac('sha256', process.env.MICROCMS_SIGNATURE)
-    .update(JSON.stringify(req.body))
-    .digest('hex')
-  const expectedSignature_dig = crypto
-    .createHmac('sha256', process.env.MICROCMS_SIGNATURE)
-    .update(JSON.stringify(req.body))
-  const expectedSignature_dig_up = crypto.createHmac('sha256', process.env.MICROCMS_SIGNATURE)
+  const expectedSignature = crypto.createHmac('sha256', 'abc').update(JSON.stringify(req.body)).digest('hex')
 
   // eslint-disable-next-line no-console
   console.log('expectedSignature', expectedSignature)
-  // eslint-disable-next-line no-console
-  console.log('expectedSignature_dig', expectedSignature_dig)
-  // eslint-disable-next-line no-console
-  console.log('expectedSignature_dig_up', expectedSignature_dig_up)
   // eslint-disable-next-line no-console
   console.log('Buffer.from(expectedSignature)', Buffer.from(expectedSignature))
   // eslint-disable-next-line no-console
   console.log('signature', signature)
   // eslint-disable-next-line no-console
-  console.log(`req.headers['x-microcms-signature']`, Buffer.from(signature?.toString() || ''))
-
-  // if check http request header named x-microcms-signature
-  if (req.headers['x-microcms-signature'] !== process.env.MICROCMS_SIGNATURE) {
-    return res.status(401).send('Unauthorized')
-  }
-  // eslint-disable-next-line no-console
-  console.log('401ではない')
+  console.log(`req.headers['x-microcms-signature']`, Buffer.from(signature))
 
   try {
     await res.revalidate('/projects')
