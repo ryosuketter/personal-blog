b12ddff4337436ac4600091940a6d231455d391e
ryosuketter <r.shimizu.yale@gmail.com>
2023-05-09 07:53:14 +0900

    added proxy url

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const rewrites = async () => {
       destination: 'https://example.com/blog'
     },
     {
+      source: '/login_88273',
+      destination: 'https://hakadori-soudan.com/login_88273'
+    },
+    {
       source: '/:path*',
       destination: 'https://legacy.hakadori-soudan.com/:path*'
     },
