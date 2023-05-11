135de86748460fa7c2d251083709f00a3bb984c5
ryosuketter <r.shimizu.yale@gmail.com>
2023-05-11 16:19:19 +0900

    feat: Firebase JavaScript SDKの接続

---
+++ src/lib/firebase/client.ts
@@ -0,0 +1,19 @@
+import { getApps, initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAI,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+}
+
+if (!getApps()?.length) initializeApp(firebaseConfig)
+
+export const db = getFirestore()
+export const storage = getStorage()
+export const auth = getAuth()
