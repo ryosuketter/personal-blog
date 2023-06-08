32a395bd13a941090ac7ea1a4df5c3172f9c15bd
ryosuketter <r.shimizu.yale@gmail.com>
2023-06-09 00:00:00 +0900

    コメント削除

--- src/features/stores/context/auth.tsx
+++ src/features/stores/context/auth.tsx
@@ -15,32 +15,26 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
-        // ログインしていた場合、ユーザーコレクションからユーザーデータを参照
         const userRef = collection(db, 'users')
         const docRef = doc(userRef, user.uid)
         const snap = await getDoc(docRef)
 
         if (snap.exists()) {
-          // ユーザーデータを取得して格納
           const userData = snap.data() as User
           setUser(userData)
         } else {
-          // ユーザーが未作成の場合、新規作成して格納
           const userData: User = {
             uid: user.uid,
             createdAt: new Date()
           }
-          // Firestoreにユーザーデータを保存
           await setDoc(docRef, userData)
           setUser(userData)
         }
       } else {
-        // ログインしていない場合、ユーザー情報を空にする
         setUser(null)
       }
     })
 
-    // このコンポーネントが不要になったら監視を終了する
     return unsubscribe
   }, [])
 
